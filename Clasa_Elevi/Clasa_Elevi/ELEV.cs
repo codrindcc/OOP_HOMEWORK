3240f331a9a8e7a4c898b094d4ab03a199d0eb29
codrindcc <56298801+codrindcc@users.noreply.github.com>
2020-06-10 13:30:33 +0300

    Add files via upload

---
+++ Clasa_Elevi/Clasa_Elevi/ELEV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Elevi
+{
+    class ELEV
+    {
+        public string name;
+        string surname;
+        public float media;
+
+        public ELEV(string data)
+        {
+            string[] local = data.Split(' ');
+            this.name = local[0];
+            this.surname = local[1];
+
+            int NotesNr = int.Parse(local[2]);
+            for (int i = 0; i < NotesNr; i++)
+                media += float.Parse(local[i + 3]);
+            media = media / (float)NotesNr;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + media.ToString("0.00");
+        }
+    }
+}
---
+++ Clasa_Elevi/Clasa_Elevi/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Clasa_Elevi
+{
+    class Program
+    {
+        public static List<ELEV> elevi = new List<ELEV>();
+        static void Main(string[] args)
+        {
+            Load();
+
+            for (int i = 0; i < elevi.Count - 1; i++)
+            {
+                for (int j = i + 1; j < elevi.Count; j++)
+                    if ((elevi[i].media < elevi[j].media) || ((elevi[i].media < elevi[j].media) && (StringCompare(elevi[i].name, elevi[j].name) < 0)))
+                    {
+                        ELEV aux = elevi[i];
+                        elevi[i] = elevi[j];
+                        elevi[j] = aux;
+                    }
+            }
+
+            elevi.Sort((x, y) =>
+            {
+                var result = y.media.CompareTo(x.media);
+                if (result == 0)
+                    result = y.name.CompareTo(x.name);
+                return result;
+            });
+
+            Save();
+        }
+
+        public static void Save()
+        {
+            TextWriter dataSave = new StreamWriter(@"..\..\data.out");
+            foreach (ELEV e in elevi)
+                dataSave.WriteLine(e.ToString());
+            dataSave.Close();
+        }
+        public static void Load()
+        {
+            TextReader dataLoad = new StreamReader(@"..\..\data.txt");
+            string buffer;
+            while ((buffer = dataLoad.ReadLine()) != null)
+                elevi.Add(new ELEV(buffer));
+            dataLoad.Close();
+        }
+        public static int StringCompare(string A, string B)
+        {
+            if (A.Length < B.Length)
+                return -1;
+            else
+                if (A.Length > B.Length)
+                return 1;
+            else
+            {
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (A[i] < B[i])
+                        return -1;
+                    else if (A[i] > B[i])
+                        return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
+
